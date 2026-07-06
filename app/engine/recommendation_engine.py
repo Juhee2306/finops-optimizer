85d22119784d89c4ecb2ce253df6adb7079cca72
Juhee Lavanya <juheelavanya@gmail.com>
2026-07-06 13:41:31 +0000

    feat: add EBS resource analysis

--- app/engine/recommendation_engine.py
+++ app/engine/recommendation_engine.py
@@ -1,4 +1,5 @@
 from analyzers.ec2_analyzer import EC2Analyzer
+from analyzers.ebs_analyzer import EBSAnalyzer
 from services.aws_client import AWSClient
 from services.cloudwatch_service import CloudWatchService
 
@@ -9,11 +10,15 @@ class RecommendationEngine:
         self.aws = AWSClient()
         self.cloudwatch = CloudWatchService()
         self.ec2_analyzer = EC2Analyzer()
+        self.ebs_analyzer = EBSAnalyzer()
 
-    def analyze_ec2_instances(self):
+    def analyze(self):
 
         findings = []
 
+        # -------------------------
+        # Analyze EC2 Instances
+        # -------------------------
         instances = self.aws.get_ec2_instances()
 
         for instance in instances:
@@ -27,6 +32,7 @@ class RecommendationEngine:
                 findings.extend(
                     self.ec2_analyzer.analyze(instance, None)
                 )
+
                 continue
 
             cpu = self.cloudwatch.get_average_cpu_utilization(
@@ -42,6 +48,17 @@ class RecommendationEngine:
 
             findings.extend(
                 self.ec2_analyzer.analyze(instance, cpu)
+            )
+
+        # -------------------------
+        # Analyze EBS Volumes
+        # -------------------------
+        volumes = self.aws.get_ebs_volumes()
+
+        for volume in volumes:
+
+            findings.extend(
+                self.ebs_analyzer.analyze(volume)
             )
 
         return findings
