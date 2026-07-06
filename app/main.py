7aeaab9740fdaaeebf4a14e648b56adcd422f1d6
Juhee Lavanya <juheelavanya@gmail.com>
2026-07-06 13:17:34 +0000

    feat: generate JSON optimization report

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
+import json
+from dataclasses import asdict
+
 from engine.recommendation_engine import RecommendationEngine
 
 engine = RecommendationEngine()
 
 findings = engine.analyze_ec2_instances()
 
+report = []
+
 for finding in findings:
-    print(finding)
+    report.append(asdict(finding))
+
+print(json.dumps(report, indent=4))
+
+with open("report.json", "w") as file:
+    json.dump(report, file, indent=4)
+
+print("\nReport saved as report.json")
