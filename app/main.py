7d519c77d0d675ea22357ef25a84a0bd9e52ad8a
Juhee Lavanya <juheelavanya@gmail.com>
2026-07-07 14:03:47 +0000

    Revert "your commit message"
    This reverts commit 46bf3fdf63e02b27a91cd722b8fdfb4e9c5a001b.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import os
 import json
 from dataclasses import asdict
 
@@ -16,9 +15,7 @@ report = [asdict(finding) for finding in findings]
 
 print(json.dumps(report, indent=4))
 
-os.makedirs("reports", exist_ok=True)
-
-with open("reports/report.json", "w") as file:
+with open("report.json", "w") as file:
     json.dump(report, file, indent=4)
 
 print("\nReport saved as report.json")
